90b3a5f7dbeaf4e903a3120396b3b90c4fd7969f
Ümmügülsüm Çamoğlu <69233551+gulsumcamoglu@users.noreply.github.com>
2023-03-10 23:04:00 +0300

    Update and rename Basics_Python_Project to Basics_Python_Project.py

--- Basics_Python_Project.py
+++ Basics_Python_Project.py
@@ -1,7 +1,7 @@
 ## Question-1 : Write a function that flattening the given matrix
 Input : input: [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 Output : output: [1,'a','cat',2,3,'dog',4,5]
-output = []
+output = [] 
 def flatten(input):
     for i in input:
         if isinstance(i,list):
